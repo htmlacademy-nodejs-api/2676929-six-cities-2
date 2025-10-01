ca00dbb6ea602f06f4c54815a253c52661e1aac2
Михаил Виноградов <sin_kor@MacBook-Pro-Mihail.local>
2025-10-01 22:31:01 +0200

    feat: made re-export for cli commands

---
+++ src/cli/commands/index.ts
@@ -0,0 +1,5 @@
+export * from "./command-parser.ts";
+export * from "./command.interface.ts";
+export * from "./help.command.ts";
+export * from "./import.command.ts";
+export * from "./version.command.ts";
